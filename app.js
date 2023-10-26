20d70e4b87bba4317848d22739d19cf4b171ba60
Santhosh Reddy Gurram <santhoshreddy4148@gmail.com>
2023-10-26 00:13:51 -0500

    Added choose

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var VechileRouter = require('./routes/Vechile');
 var gridbuildRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Vechile', VechileRouter);
 app.use('/board', gridbuildRouter);
+app.use('/choose', chooseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
